3f6f58749e67ce47d8c9906d5a916fece959f97e
JP Bourget <jp@syncurity.net>
2020-01-15 16:29:16 -0500

    fix .github

--- tools/add_gh_config.sh
+++ tools/add_gh_config.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-# Script which copies over new sample circle config to all the pack repos
+# Script which copies the .github directory to enable templates and other github dotfile
+# functionality to all the pack repos
 # NOTE: It assumes packs repos are cloned in a local directory where the script runs
 
 for PACK_DIR in syncurity-*; do
@@ -8,10 +9,13 @@ for PACK_DIR in syncurity-*; do
     PACK_DIR=`realpath ${PACK_DIR}`
 
     cd ${PACK_DIR}
-    mkdir .github
+    if [[ ! -d ".github" ]]; then
+        echo "Creating .github directory"
+        mkdir .github
+    fi
     cp ~/repos/ci/.github/* ${PACK_DIR}/.github/.
     git add .github/*
-    git commit -m "Update CircleCI config from ci repo."
+    git commit -m "Update .github templates from ci repo."
     git push origin master
     cd ..
 done
